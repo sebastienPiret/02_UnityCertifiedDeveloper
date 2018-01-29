2ebc4e7ca9483c9788f6ce9858d036fe08dfafe8
Touten3D-PC\Touten3D <sebpiret@hotmail.com>
2018-01-29 01:09:51 +0100

    2- Editor Interface

---
+++ Assets/Script/LogMessages.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World!");
+		Debug.LogError("Shit!");
+		Debug.LogWarning("Don't do that youmf");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
